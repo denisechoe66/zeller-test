668f1ae7ab10e472b400a9d4880429bb8fee6b3c
Denise Choe <denise_choe@racv.com.au>
2021-10-10 19:01:44 +1100

    update readme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+import App from './App';
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+describe('Fetch Customer Data', () => {
+  it('return customer data', async () => {
+    const mockResponse = {
+      value: {
+        data: {
+          name: 'David Miller',
+          role: 'ADMIN',
+          email: 'david@gmail.com',
+          id: '73bae2af-4fa4-4023-8829-1034604e7590'
+        }
+      }
+    };
+    console.log('mock= ', mockResponse);
+  });
 });
